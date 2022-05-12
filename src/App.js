30cc7d1bc29588f639a45676b563dd57904eb908
Damian Wasilewski <230032@edu.p.lodz.pl>
2022-05-12 16:24:07 +0200

    Add react router and game's subpages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import words from "./api.json";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Game from "./pages/Game";
+import Results from "./pages/Results";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
